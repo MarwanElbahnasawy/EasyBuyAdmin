d323e5de2882a963b8cbbd55b655608dd3ebb5a0
Marwan Elbahnasawy <96259021+MarwanElbahnasawy@users.noreply.github.com>
2023-06-03 23:58:31 +0300

    Initial Commit

---
+++ EasyBuyAdmin/EasyBuyAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyBuyAdminApp.swift
+//  EasyBuyAdmin
+//
+//  Created by Marwan Elbahnasawy on 03/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct EasyBuyAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
